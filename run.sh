abd3dd5f54d24d6c4028080c18daf6202cf91b92
Brandon Karai <b.karai1996@gmail.com>
2021-11-21 15:18:00 -0500

    added in run script

---
+++ run.sh
@@ -0,0 +1,52 @@
+#/usr/bin/env sh
+
+APP_NAME="outwit";
+OS_TYPE=$(uname);
+
+function load_env_file() {
+    set -o allexport;
+    source $1;
+    set +o allexport;
+}
+
+function log() {
+    echo "### outwit ### - ${1}";
+    return $?;
+}
+
+function is_os_type() {
+    if [ $1 == $(uname) ]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+function is_mac() {
+    is_os_type 'Darwin';
+    return $?;
+}
+
+function is_linux() {
+    is_os_type 'Linux';
+    return $?;
+}
+
+# Either "development" or "production"
+ENV=${1:-development};
+
+log "Running app in ${ENV} mode";
+log "Using configuration for OS: ${OS_NAME}}";
+
+if [[ is_mac ]]; then
+    OS_FILE_NAME_PART="mac";
+elif [[ is_linux ]]; then
+    OS_FILE_NAME_PART="linux";
+else
+    log "You are on an unsupported platform";
+    return 1;
+fi
+
+load_env_file ".${OS_FILE_NAME_PART}.${ENV}.env";
+
+docker-compose up ${@:2};
