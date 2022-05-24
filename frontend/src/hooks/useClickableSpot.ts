2bf1fde6fd5020757ea202bde87e26f0a54462d3
Brandon Karai <b.karai1996@gmail.com>
2022-05-24 00:09:08 -0400

    added sound effect (partially complete)

--- frontend/src/hooks/useClickableSpot.ts
+++ frontend/src/hooks/useClickableSpot.ts
@@ -1,11 +1,16 @@
 import { useCallback, useContext } from "react";
 import { GameContext, selectPiece } from "context/game";
 import { Spot } from 'model'
+import click from 'assets/click.wav';
+
+function playClickSound() {
+  new Audio(click).play();
+}
 
 export function useClickableSpot(spot: Spot) {
   const location = spot.getLocation();
 
   const { dispatch } = useContext(GameContext);
-  return useCallback(() => dispatch(selectPiece(location)),
+  return useCallback(() => { playClickSound(); dispatch(selectPiece(location)) },
   [dispatch, location]);
 }
