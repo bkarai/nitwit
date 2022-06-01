a7c3f9f5a920869bf6a6853cfb4398e8a730e416
Brandon Karai <b.karai1996@gmail.com>
2022-06-01 04:05:53 -0400

    cleaned some stuff up

--- frontend/src/components/business/Layout/Header/index.tsx
+++ frontend/src/components/business/Layout/Header/index.tsx
@@ -27,10 +27,10 @@ export const Header = React.memo(function() {
         <Instructions />
       </Dialog>
       <Box component='nav' height="100%" width="100%" className="navbar navbar-light bg-light ps-3 pe-3" display="flex">
-        <Tooltip title="Home" arrow>
+        <Tooltip title="Home" arrow enterDelay={500} enterNextDelay={2000}>
           <Link className="navbar-brand" to="/">Outwit</Link>
         </Tooltip>
-        <Tooltip title="Click here to learn how to play" arrow>
+        <Tooltip title="Click here to learn how to play" arrow enterDelay={500} enterNextDelay={2000}>
           <Button onClick={handleClickHelp}> 
             <Box>
               <InfoIcon/>
