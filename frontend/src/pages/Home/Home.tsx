1ff3cb38d65fd1117c8a99fd045dda023ac54403
Brandon Karai <b.karai1996@gmail.com>
2022-06-01 08:33:00 -0400

    fixed up some styling

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -7,13 +7,13 @@ export function Home() {
     <ContentWrapper boxProps={{ height: '100%' }}>
       <h1>Play Outwit</h1>
       <p className="lead mt-3">Outwit is a fun two-player strategy game that was originally released in 1975. I decided it would be a fun project to build a web version of the game. If you're interested in learning more about the game, <a href="https://boardgamegeek.com/boardgame/4696/outwit">this</a> is a good resource.</p>
-      <Box mt={3} display='flex' justifyContent='center' gap="24px">
+      <Box height={'50%'} maxHeight="50px" mt={3} display='flex' justifyContent='center' gap="24px">
         <Link to={Instructions.path} style={{ width: '35%', maxWidth: '300px' }}>
-          <Button style={{ width: '100%' }}>
+          <Button style={{ width: '100%', height: '100%' }}>
               Learn how to play Outwit
           </Button>
         </Link>
-        <NewGameButton buttonProps={{style: { width: '35%', maxWidth: '300px', alignSelf: 'center' }}}/>
+        <NewGameButton buttonProps={{style: { width: '35%', maxWidth: '300px' }}}/>
       </Box>
     </ContentWrapper>
   );
