05604dfd7d7720b25088a8ae4ea46aeb24e52ca2
Brandon Karai <b.karai1996@gmail.com>
2022-06-09 23:11:36 -0400

    changed div to Box

--- frontend/src/components/business/WaitingForPlayer/index.tsx
+++ frontend/src/components/business/WaitingForPlayer/index.tsx
@@ -12,9 +12,9 @@ import {
 } from 'react-share';
 
 import { useGameAccessKey } from 'hooks';
-import { Button } from 'components';
+import { Button, Box } from 'components';
 
-export const IconWrapper = styled.div({
+export const IconWrapper = styled(Box)({
   svg: {
     borderRadius: '50%',
   },
@@ -34,19 +34,19 @@ export function WaitingForPlayer() {
   };
 
   return (
-    <div>
+    <Box>
       <h1 className="display-2">
         Waiting for opponent to join
       </h1>
       <h2 className="mt-5">
         Copy the link below to invite a player to your game
       </h2>
-      <div className="input-group mb-5 mt-5" style={{ height: 'auto' }}>
+      <Box className="input-group mb-5 mt-5" height='auto'>
         <output className="form-control">
           {linkToJoinGame}
         </output>
         <Button className="input-group-append" style={{ width: '25%' }} onClick={copyToClipboard}>Copy</Button>
-      </div>
+      </Box>
       <IconWrapper>
         <EmailShareButton className="me-2" openShareDialogOnClick url={linkToJoinGame} subject="Join a game of Outwit!" body="Hello, you've been invited to a game of Outwit. Copy and paste the following link in your browser address bar to join the game: ">
           <EmailIcon />
@@ -61,6 +61,6 @@ export function WaitingForPlayer() {
           <WhatsappIcon />
         </WhatsappShareButton>
       </IconWrapper>
-    </div>
+    </Box>
   );
 };
