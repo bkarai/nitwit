bdd73e17f657976632dc7a8aa20f82eaa6c1d585
Brandon Karai <b.karai1996@gmail.com>
2022-06-19 19:51:26 -0400

    added in google form link

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -14,7 +14,7 @@ export function Home() {
   return (
     <ContentWrapper boxProps={{ height: '100%' }}>
       <h1>Play Nitwit</h1>
-      <p className="lead mt-3">Nitwit is a fun two-player strategy game that is inspired by the game Outwit (1975).<br />I decided it would be a fun project to build an improved version of the original. If you're interested in learning more about the original game, <a href="https://boardgamegeek.com/boardgame/4696/outwit" target="_blank" rel="noreferer">this</a> is a good resource.</p>
+      <p className="lead mt-3">Nitwit is a fun two-player strategy game that is inspired by the game Outwit (1975).<br />I decided it would be a fun project to build an improved version of the original. If you're interested in learning more about the original game, <a href="https://boardgamegeek.com/boardgame/4696/outwit" target="_blank" rel="noreferrer">this</a> is a good resource.</p>
       <Box height={'50%'} maxHeight="50px" mt={4} display='flex' flexDirection={isMobile ? 'column' : 'row'} justifyContent='center' gap="24px" alignItems='center'>
         <Link to={Instructions.path} style={{ width: isMobile ? '75%' : '35%', maxWidth: '300px' }}>
           <Button style={{ width: '100%', height: '100%' }}>
@@ -22,6 +22,10 @@ export function Home() {
           </Button>
         </Link>
         <Button onClick={handleClickNewGame} style={{ width: isMobile ? '75%' : '35%', maxWidth: '300px' }}>New Game</Button>
+      </Box>
+      <Box mt={5}>
+        <h3>Have you noticed a bug or would you like to make a suggestion for improvement?</h3>
+        <a href="https://forms.gle/KfVZ1ACtS6eobWUKA" target="_blank" rel="noreferrer">Fill out the feedback form here</a>
       </Box>
     </ContentWrapper>
   );
