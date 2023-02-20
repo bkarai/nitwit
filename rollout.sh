aa0f9574fc004018b561c6f6b8e725d303c6d0cc
Brandon Karai <b.karai1996@gmail.com>
2023-02-20 14:13:26 -0500

    fixed rollout script to add build

--- rollout.sh
+++ rollout.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+echo "-- BUILDING IMAGES --"
+
+read -p "Build Proxy? (y/n): " INPUT
+
+if [ "$INPUT" = y ]; then
+  docker-compose -f docker-compose.production.yml build proxy;
+fi
+
+read -p "Build Frontend? (y/n)" INPUT
+
+if [ "$INPUT" = y ]; then
+  docker-compose -f docker-compose.production.yml build frontend;
+fi
+
+read -p "Build Backend? (y/n)" INPUT
+
+if [ "$INPUT" = y ]; then
+  docker-compose -f docker-compose.production.yml build backend;
+fi
+
 echo "-- DOWNING COINTAINERS --"
 docker-compose -f docker-compose.production.yml down
 
