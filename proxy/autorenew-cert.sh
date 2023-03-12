8c532f3d60707d40e0ea7ad4a01d63b11fa3f157
Brandon Karai <b.karai1996@gmail.com>
2023-03-12 16:46:41 -0400

    added autorenewal

---
+++ proxy/autorenew-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+CHALLENGE_DIR="/var/www/certbot";
+MY_EMAIL="b.karai1996@gmail.com";
+
+certbot certonly --webroot --webroot-path "${CHALLENGE_DIR}" -d play-nitwit-game.com -d www.play-nitwit-game.com --non-interactive --agree-tos --email "${MY_EMAIL}"
