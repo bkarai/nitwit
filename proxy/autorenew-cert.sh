39951ad5873e84be61a08847aa5eff221e19ed78
Brandon Karai <b.karai1996@gmail.com>
2023-04-02 17:48:47 -0400

    fixed some certificate renewal stuff

--- proxy/autorenew-cert.sh
+++ proxy/autorenew-cert.sh
@@ -2,7 +2,4 @@
 
 set -e
 
-CHALLENGE_DIR="/var/www/certbot";
-MY_EMAIL="b.karai1996@gmail.com";
-
-certbot certonly --force-renewal --cert-name play-nitwit-game.com --webroot --webroot-path "${CHALLENGE_DIR}" -d play-nitwit-game.com -d www.play-nitwit-game.com --non-interactive --agree-tos --email "${MY_EMAIL}"
+certbot renew --cert-name play-nitwit-game.com
