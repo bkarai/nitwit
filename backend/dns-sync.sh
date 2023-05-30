b7be4c8ccbcc7cf2a6a39ba59653f0ca21e1a44a
Brandon Karai <b.karai1996@gmail.com>
2023-05-30 06:20:04 -0400

    added dns sync shell script

---
+++ backend/dns-sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+cd /home/brandon/projects/nitwit
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/opt"
+
+docker-compose -f docker-compose.production.yml exec backend yarn start:dns-sync
