24ddc2430906e1a60edc8b4c9f6f640a3129c7b8
Brandon Karai <b.karai1996@gmail.com>
2024-03-12 01:26:54 -0400

    fix bug preventing user from viewing instructions when clicking button on homepage

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { useCallback } from "react";
 import { Box, Button, ContentWrapper } from "components";
 import { useIsMobile } from "hooks";
-import { Instructions } from "pages/Instructions";
 import { Link, useNavigate } from 'react-router-dom';
 
 export function Home() {
@@ -16,7 +15,7 @@ export function Home() {
       <h1>Play Nitwit</h1>
       <p className="lead mt-3">Nitwit is a fun two-player strategy game that is inspired by the game Outwit (1975).<br />I decided it would be a fun project to build an improved version of the original. If you're interested in learning more about the original game, <a href="https://boardgamegeek.com/boardgame/4696/outwit" target="_blank" rel="noreferrer">this</a> is a good resource.</p>
       <Box height={'50%'} maxHeight="50px" mt={4} display='flex' flexDirection={isMobile ? 'column' : 'row'} justifyContent='center' gap="24px" alignItems='center'>
-        <Link to={Instructions.path} style={{ width: isMobile ? '75%' : '35%', maxWidth: '300px' }}>
+        <Link to='#help' style={{ width: isMobile ? '75%' : '35%', maxWidth: '300px' }}>
           <Button style={{ width: '100%', height: '100%' }}>
               Learn how to play Nitwit
           </Button>
